80b0c29d7179d43155ecc1f26562fff3fbe8cc3c
Theerakarn <worknarair6@gmail.com>
2025-04-16 19:35:13 +0700

    update brand

--- apps/web-admin/app/routes/brands.$id.tsx
+++ apps/web-admin/app/routes/brands.$id.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "@remix-run/react"
 import { db } from "@workspace/db/src"
 import { brands } from "@workspace/db/src/schema"
 import { eq } from "drizzle-orm"
+import { getBrand } from "~/action/brand"
 import { BrandForm } from "~/components/brands/brand-form"
 import MainLayout from "~/layouts/MainLayout"
 
@@ -36,14 +37,7 @@ export async function loader({ params }: { params: { id: string } }) {
     throw new Response("Invalid brand ID", { status: 400 })
   }
 
-  // const [brand] = await db.select().from(brands).where(eq(brands.brandId, brandId)).limit(1);
-
-  const brand = {
-    brandId: brandId,
-    name: "Sample Brand",
-    description: "This is a sample brand description.",
-    logoUrl: "https://example.com/logo.png",
-  }
+  const brand = await getBrand(brandId)
 
   if (!brand) {
     throw new Response("Brand not found", { status: 404 })

88bece59f92550230d56332e3aedd05df776ab85
Theerakarn <worknarair6@gmail.com>
2025-04-22 11:28:46 +0700

    feat: update product page loader to fetch brands and categories from API

--- apps/web-admin/app/routes/products.new.tsx
+++ apps/web-admin/app/routes/products.new.tsx
@@ -2,6 +2,8 @@ import { db } from "@workspace/db/src"
 import { useLoaderData } from "@remix-run/react"
 import { ProductForm } from "~/components/products/product-form"
 import MainLayout from "~/layouts/MainLayout"
+import { getBrands } from "~/action/brand"
+import { getCategories } from "~/action/category"
 
 export default function NewProductPage() {
   const { brandsData, categoriesData } = useLoaderData<typeof loader>()
@@ -21,11 +23,13 @@ export default function NewProductPage() {
 }
 
 export async function loader() {
-  // const brandsData = await db.select().from(brands)
-  // const categoriesData = await db.select().from(categories)
+  const [brands, categories] = await Promise.all([
+    getBrands(),
+    getCategories()
+  ])
 
   return {
-    brandsData: [],
-    categoriesData: [],
+    brandsData: brands,
+    categoriesData: categories,
   }
 }
